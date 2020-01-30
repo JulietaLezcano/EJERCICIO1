22a6779c385862d8459d54dc927f170a5f58d294
JulietaLezcano <60350457+JulietaLezcano@users.noreply.github.com>
2020-01-30 09:11:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Ejercicio1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar fecha I : dd/mm/yy");
+            string FechaI= Console.ReadLine();
+            Console.WriteLine("Ingresar fecha II: dd/mm/yy ");
+            string FechaII= Console.ReadLine();
+            DateTime fechaI= DateTime.Parse(FechaI);
+            DateTime fechaII= DateTime.Parse(FechaII);
+            
+            TimeSpan diff= fechaII.Subtract(fechaI);
+             Console.WriteLine("Dias de diferencia:" +" "+ diff);
+             Console.WriteLine("Semanas de diferencia:" +" "+ diff/7);
+
+            
+
+
+            
+        }
+   }
+}
